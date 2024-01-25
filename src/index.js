8136d8462a3021098f52599f6afac0eb1f17c695
Lotanna <tannaloamobi@gmail.com>
2024-01-25 22:56:23 +0000

    added prompt questions

--- src/index.js
+++ src/index.js
@@ -11,17 +11,86 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
-inquirer.prompt([
-  {
-    type: "input",
-    name: "name",
-    message: "What's your name?",
-  },
-]);
-inquirer.prompt([
-  {
-    type: "input",
-    name: "name",
-    message: "What's your name?",
-  },
-]);
+function Manager() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Please input your name.",
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "Please input your id number.",
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "Please input your email.",
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "Please input your office number.",
+    },
+  ]);
+}
+function Engineer() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Please input your name.",
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "Please input your id number.",
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "Please input your email.",
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "Please input your gitHub username.",
+    },
+  ]);
+}
+function Intern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please input your name.",
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "Please input your id number.",
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "Please input your email.",
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "Please input your school.",
+      },
+    ])
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
